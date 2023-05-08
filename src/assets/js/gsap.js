a424563e87ce73eea6054a923cd0f6ea68c0001e
long.dongoc <long.dongoc@cybereye.vn>
2023-05-08 08:38:34 +0700

    update

--- src/assets/js/gsap.js
+++ src/assets/js/gsap.js
@@ -5,35 +5,38 @@ import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 const sections = gsap.utils.toArray("section");
-gsap.set(sections, { opacity: 0 });
+// gsap.set(sections, { opacity: 0 });
 
-sections.forEach((section) => {
-	ScrollTrigger.create({
-		trigger: section,
-		start: "top center",
-		end: "bottom center",
+// sections.forEach((section) => {
+// 	ScrollTrigger.create({
+// 		trigger: section,
+// 		start: "top center",
+// 		end: "bottom center",
 
-		onEnter: () => {
-			gsap.to(section, { opacity: 1 });
-			//   video.play();
-		},
-		// onEnterBack: () => (section, { opacity: 0 }),
-		// onLeave: () => (section, { opacity: 0 }),
-		// onLeaveBack: () => (section, { opacity: 0 }),
-	});
-});
+// 		onEnter: () => {
+// 			gsap.to(section, { opacity: 1 });
+// 			//   video.play();
+// 		},
+// 		onEnterBack: () => (section, { opacity: 1 }),
+// 		onLeave: () => (section, { opacity: 1 }),
+// 		onLeaveBack: () => (section, { opacity: 1 }),
+// 	});
+// });
 // usage:
 // usage:
 ScrollTrigger.batch([".card", ".item", ".Marquee-tag"], {
+	autoAlpha:0,
 	interval: 0.1, // time window (in seconds) for batching to occur. The first callback that occurs (of its type) will start the timer, and when it elapses, any other similar callbacks for other targets will be batched into an array and fed to the callback. Default is 0.1
 	batchMax: 4, // maximum batch size (targets)
 	onEnter: (batch) =>
 		gsap.to(batch, { autoAlpha: 1, stagger: 0.15, overwrite: true }),
-	onLeave: (batch) => gsap.set(batch, { autoAlpha: 0, overwrite: true }),
+	onLeave: (batch) => gsap.set(batch, { autoAlpha: 1, overwrite: true }),
 	onEnterBack: (batch) =>
-		gsap.to(batch, { autoAlpha: 1, stagger: 0.15, overwrite: true }),
+		gsap.to(batch, { autoAlpha: 0, stagger: 0.15, overwrite: true }),
 	onLeaveBack: (batch) => gsap.set(batch, { autoAlpha: 0, overwrite: true }),
-	// you can also define things like start, end, etc.
+	// you can also define things like start, end, etc.\
+	start: "top center",
+	end: "bottom center",
 });
 
 // menu humberger
