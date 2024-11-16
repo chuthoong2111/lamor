519d6473fa910daca505a0287727d942c19c9b8f
long.dongoc <long.dongoc@cybereye.vn>
2024-11-16 09:15:39 +0700

    update

--- src/assets/js/gsap.js
+++ src/assets/js/gsap.js
@@ -4,23 +4,48 @@ import gsap from "gsap";
 // get other plugins:
 import ScrollTrigger from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
+gsap.set([".card", ".item", ".Marquee-tag"], {y: 20, autoAlpha: 0,});
+// Sử dụng ScrollTrigger.batch để xử lý nhiều phần tử cùng một lúc
 ScrollTrigger.batch([".card", ".item", ".Marquee-tag"], {
-  opacity: 0,
+
   interval: 0.1,
-  batchMax: 4,
-  y: 100,
+  batchMax: 6,  // Số lượng phần tử tối đa được xử lý cùng lúc
   onEnter: batch => gsap.to(batch, {
-    opacity: 1,
+    autoAlpha: 1,
     y: 0,
     stagger: 0.1,
     scrollTrigger: {
-      trigger: batch,
-      start: "top center",
-      end: "bottom center",
-      toggleActions: "play none none reverse", // Đảm bảo hiệu ứng có thể lặp lại khi cuộn qua
+      trigger: batch, // Sử dụng batch như trigger
+      start: "top 90%",  // Khi phần tử đầu tiên của batch chạm tới center của viewport
+      end: "bottom 10%", // Khi phần tử cuối cùng trong batch đi qua center của viewport
+      toggleActions: "play none none reverse", // Đảm bảo hiệu ứng lặp lại khi cuộn lên và xuống
     }
   }),
 });
+
+
+// Chọn tất cả các phần tử có class '.my-class-1', '.my-class-2', '.my-class-3', ...
+// gsap.utils.batch(['.card", ".item", ".Marquee-tag'], (elements) => {
+//   gsap.fromTo(
+//     elements,
+//     {
+//       opacity: 0,
+//       y: 100,
+//     },
+//     {
+//       opacity: 1,
+//       y: 0,
+//       scrollTrigger: {
+//         trigger: elements, // Các phần tử này sẽ là trigger
+//         start: 'top 90%',   // Bắt đầu khi phần tử cách 2% từ trên cùng
+//         end: 'top 10%',     // Kết thúc khi phần tử đi qua điểm 10% từ trên
+//         scrub: true,        // Đồng bộ hiệu ứng với cuộn chuột
+//         markers: false,     // Tắt markers (có thể bật khi debug)
+//         toggleActions: 'restart pause resume reverse', // Lặp lại hiệu ứng khi cuộn lên hoặc xuống
+//       }
+//     }
+//   );
+// });
 
 
 // menu humberger
